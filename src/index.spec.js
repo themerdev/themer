fe72f06ef168fb1911421b79a9b584a35ab1fbed
Matt Swensen <swensen.matthew@gmail.com>
2016-12-31 23:12:55 -0700

    Using themer-colors-default for tests.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,45 +1,9 @@
 import { render } from './index';
+import { colors } from 'themer-colors-default';
 
 describe('iTerm theme generator', () => {
 
-  const darkSet = {
-    accent0: '#000000',
-    accent1: '#000000',
-    accent2: '#000000',
-    accent3: '#000000',
-    accent4: '#000000',
-    accent5: '#000000',
-    accent6: '#000000',
-    accent7: '#000000',
-    shade0: '#000000',
-    shade1: '#000000',
-    shade2: '#000000',
-    shade3: '#000000',
-    shade4: '#000000',
-    shade5: '#000000',
-    shade6: '#000000',
-    shade7: '#000000',
-  };
-  const lightSet = {
-    accent0: '#ffffff',
-    accent1: '#ffffff',
-    accent2: '#ffffff',
-    accent3: '#ffffff',
-    accent4: '#ffffff',
-    accent5: '#ffffff',
-    accent6: '#ffffff',
-    accent7: '#ffffff',
-    shade0: '#ffffff',
-    shade1: '#ffffff',
-    shade2: '#ffffff',
-    shade3: '#ffffff',
-    shade4: '#ffffff',
-    shade5: '#ffffff',
-    shade6: '#ffffff',
-    shade7: '#ffffff',
-  };
-
-  const promisedFiles = render({ dark: darkSet, light: lightSet }, {});
+  const promisedFiles = render(colors, {});
 
   it('should render two files when given a color set with both a light and a dark theme', async () => {
     const files = await Promise.all(promisedFiles);
