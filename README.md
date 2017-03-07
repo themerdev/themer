148467d2fa7c86fd1d969077a1913df6d865bd13
Matt Swensen <swensen.matthew@gmail.com>
2017-03-06 22:22:40 -0700

    Rendering actual packages instead of a single file.

--- README.md
+++ README.md
@@ -12,13 +12,16 @@ Then pass `themer-hyper` as a `-t` (`--template`) arg to `themer`:
 
     themer -c my-colors.js -t themer-hyper -o gen
 
-`themer-hyper` will generate a `themer-hyper-dark.js` or `themer-hyper-light.js` (or both), depending on which color set you passed to `themer`.
+`themer-hyper` will generate a `themer-hyper-dark/` or `themer-hyper-light/` directory (or both), depending on which color set you passed to `themer`, which will contain the files for a Hyper theme plugin package.
 
-To install your generated plugin, edit `~/.hyper.js` and add the path to the `localPlugins` array:
+To install your generated plugin, first symlink the outputted package directory to the Hyper local plugins directory:
+
+    ln -s <full path to themer output directory>/themer-hyper/themer-hyper-dark ~/.hyper_plugins/local/
+
+Finally, edit `~/.hyper.js` and add the package to the `localPlugins` array:
 
     ...
     localPlugins: [
-      '/path/to/themer-hyper-dark.js`,
+      'themer-hyper-dark`,
     ],
     ...
-
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,20 +1,26 @@
 import { render } from './index';
 import { colors } from 'themer-colors-default';
+import path from 'path';
 
 describe('Hyper.app theme generator', () => {
 
-  const promisedFiles = render(colors, {});
+  const promisedFiles = Promise.all(render(colors, {}));
 
-  it('should render two files when given a color set with both a light and a dark theme', async () => {
-    const files = await Promise.all(promisedFiles);
-    expect(files.length).toBe(2);
-    expect(files.some(file => /dark/.test(file.name))).toBe(true);
-    expect(files.some(file => /light/.test(file.name))).toBe(true);
+  it('should render four files when given a color set with both a light and a dark theme', async () => {
+    const files = await promisedFiles;
+    expect(files.length).toBe(4);
+    expect(files.filter(file => path.basename(file.name) === 'package.json').length).toBe(2);
+    expect(files.filter(file => path.basename(file.name) === 'index.js').length).toBe(2);
   });
 
-  it('should render files with no missing values', async () => {
-    const files = await Promise.all(promisedFiles);
+  it('should render files without any missing values', async () => {
+    const files = await promisedFiles;
     expect(files.every(file => !/undefined/.test(file.contents.toString('utf8')))).toBe(true);
+  });
+
+  it('should provide promised files whose contents are buffers', async () => {
+    const files = await promisedFiles;
+    files.forEach(file => expect(file.contents).toBeInstanceOf(Buffer));
   });
 
 });
