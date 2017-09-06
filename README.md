1bb556e21f4d43d49767a4444302464291245cfc
Matt Swensen <swensen.matthew@gmail.com>
2017-09-05 19:28:03 -0600

    Removing dependency on svg2png.

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Then pass `themer-wallpaper-octagon` as a `-t` (`--template`) arg to `themer`:
 
     themer -c my-colors.js -t themer-wallpaper-octagon -o gen
 
-`themer-wallpaper-octagon` will generate SVG and PNG wallpapers to the output directory (`gen/` in this example).
+`themer-wallpaper-octagon` will generate SVG wallpapers to the output directory (`gen/` in this example).
 
 ### Default resolutions
 
@@ -32,12 +32,3 @@ By default, `themer-wallpaper-octagon` will output wallpapers at the following s
 to which you would pass `<width>x<height>`. For example, to forego the default resolutions and generate two wallpapers, one 1024 by 768 and one 320 by 960:
 
     themer -c my-colors.js -t themer-wallpaper-octagon --themer-wallpaper-octagon-size 1024x768 --themer-wallpaper-octagon-size 320x960 -o gen
-
-## Previews
-
-The following are rendered using themer's default color set (mjswensen/themer-colors-default) and the default resolutions:
-
-![dark desktop](/assets/desktop-dark.png)
-![light desktop](/assets/desktop-light.png)
-![dark device](/assets/device-dark.png)
-![light device](/assets/device-light.png)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import svg2png from 'svg2png';
-
-const defaultBlockSize = 36;
+const backgroundPatternSize = 36;
 
 const getSizesFromOptOrDefault = opt => {
   if (opt) {
@@ -10,7 +8,7 @@ const getSizesFromOptOrDefault = opt => {
       if (results) {
         const w = parseInt(results[1], 10);
         const h = parseInt(results[2], 10);
-        const s = w / Math.round(w / defaultBlockSize);
+        const s = w / Math.round(w / backgroundPatternSize);
         return {
           w: w,
           h: h,
@@ -27,12 +25,12 @@ const getSizesFromOptOrDefault = opt => {
       {
         w: 2880,
         h: 1800,
-        s: defaultBlockSize,
+        s: backgroundPatternSize,
       },
       {
         w: 750,
         h: 1334,
-        s: defaultBlockSize,
+        s: backgroundPatternSize,
       }
     ];
   }
@@ -94,32 +92,32 @@ export const render = (colors, options) => {
       c: color,
     }));
 
-    const svgString = `
-      <svg xmlns="http://www.w3.org/2000/svg" style="background-color: ${colorSet.colors.shade0}" width="${size.w}" height="${size.h}" viewBox="${size.w / -2} ${size.h / -2} ${size.w} ${size.h}">
-        <defs>
-          <pattern id="bg" width="${size.s}" height="${size.s}" patternUnits="userSpaceOnUse" patternTransform="rotate(22.5)">
-            <path d="${getOctagonPathData(size.s)}" stroke="${colorSet.colors.shade1}" stroke-width="1" fill="none"/>
-          </pattern>
-          <pattern id="bg-large" width="${size.s * 10}" height="${size.s * 10}" patternUnits="userSpaceOnUse" patternTransform="rotate(-22.5)">
-            <path d="${getOctagonPathData(size.s * 10)}" stroke="${colorSet.colors.shade2}" stroke-width="2" fill="none"/>
-          </pattern>
-          <radialGradient id="accent-bg">
-            <stop offset="25%" stop-color="${colorSet.colors.shade0}"/>
-            <stop offset="100%" stop-color="${colorSet.colors.shade0}" stop-opacity="0"/>
-          </radialGradient>
-          ${paths.map((path, i) => `<linearGradient id="fill-${i}" x1="0" y1="0" x2="1" y2="1"><stop offset="25%" stop-color="${path.c}" stop-opacity="0"/><stop offset="90%" stop-color="${path.c}"/></linearGradient>`).join('\n')}
-        </defs>
-        <rect x="${size.w / -2}" y="${size.h / -2}" width="${size.w}" height="${size.h}" fill="url(#bg)"/>
-        <rect x="${size.w / -2}" y="${size.h / -2}" width="${size.w}" height="${size.h}" fill="url(#bg-large)"/>
-        <circle r="${radius * -4}" fill="url(#accent-bg)"/>
-        ${paths.map((path, i) => `<path d="M ${point1.x} ${point1.y} L ${point2.x} ${point2.y} L ${point3.x} ${point3.y} Z" stroke="${path.c}" stroke-width="0" fill="url(#fill-${i})" transform="rotate(${path.r})"/>`).join('\n')}
-      </svg>
-    `;
-    const basename = `themer-wallpaper-octagon-${colorSet.name}-${size.w}x${size.h}`;
-    const svgBuffer = Buffer.from(svgString, 'utf8');
-    return [
-      Promise.resolve({ name: `${basename}.svg`, contents: svgBuffer }),
-      svg2png(svgBuffer).then(pngBuffer => ({ name: `${basename}.png`, contents: pngBuffer })),
-    ];
+    return Promise.resolve({
+      name: `themer-wallpaper-octagon-${colorSet.name}-${size.w}x${size.h}.svg`,
+      contents: Buffer.from(`
+        <svg xmlns="http://www.w3.org/2000/svg" width="${size.w}" height="${size.h}" viewBox="${size.w / -2} ${size.h / -2} ${size.w} ${size.h}">
+          <defs>
+            <pattern id="bg" width="${size.s}" height="${size.s}" patternUnits="userSpaceOnUse" patternTransform="rotate(22.5)">
+              <path d="${getOctagonPathData(size.s)}" stroke="${colorSet.colors.shade1}" stroke-width="1" fill="none"/>
+            </pattern>
+            <pattern id="bg-large" width="${size.s * 10}" height="${size.s * 10}" patternUnits="userSpaceOnUse" patternTransform="rotate(-22.5)">
+              <path d="${getOctagonPathData(size.s * 10)}" stroke="${colorSet.colors.shade2}" stroke-width="2" fill="none"/>
+            </pattern>
+            <radialGradient id="accent-bg">
+              <stop offset="25%" stop-color="${colorSet.colors.shade0}"/>
+              <stop offset="100%" stop-color="${colorSet.colors.shade0}" stop-opacity="0"/>
+            </radialGradient>
+            ${paths.map((path, i) => `<linearGradient id="fill-${i}" x1="0" y1="0" x2="1" y2="1"><stop offset="25%" stop-color="${path.c}" stop-opacity="0"/><stop offset="90%" stop-color="${path.c}"/></linearGradient>`).join('\n')}
+          </defs>
+          <rect x="${size.w / -2}" y="${size.h / -2}" width="${size.w}" height="${size.h}" fill="${colorSet.colors.shade0}" />
+          <rect x="${size.w / -2}" y="${size.h / -2}" width="${size.w}" height="${size.h}" fill="url(#bg)"/>
+          <rect x="${size.w / -2}" y="${size.h / -2}" width="${size.w}" height="${size.h}" fill="url(#bg-large)"/>
+          <circle r="${radius * -4}" fill="url(#accent-bg)"/>
+          ${paths.map((path, i) => `<path d="M ${point1.x} ${point1.y} L ${point2.x} ${point2.y} L ${point3.x} ${point3.y} Z" stroke="${path.c}" stroke-width="0" fill="url(#fill-${i})" transform="rotate(${path.r})"/>`).join('\n')}
+        </svg>`,
+        'utf8'
+      ),
+    });
   })));
+
 };
