6f2753200476a17e7a2f1653474844d64a13d12d
Matt Swensen <swensen.matthew@gmail.com>
2017-09-16 22:25:46 -0600

    Basic infrastructure for generating the theme.

--- index.js
+++ index.js
@@ -1,57 +1,64 @@
 const path = require('path');
-const {mapValues} = require('lodash');
+const {capitalize, flattenDeep, mapValues} = require('lodash');
 const Color = require('color');
 const {version} = require('./package.json');
+const {colorPixel} = require('./image');
 
-const formatColorSet = colors =>
-  mapValues(colors, hex => Color(hex).rgb().array());
+const formatColorSet = colors => mapValues(colors, hex => Color(hex));
+
+const themeName = colorSet => `Themer ${colorSet.name}`;
+const themeDirectory = themeName;
+
+const images = {
+  theme_frame: 'frame.png',
+  theme_frame_inactive: 'frame-inactive.png',
+  theme_frame_incognito: 'frame-incognito.png',
+  theme_frame_incognito_inactive: 'frame-incognito-inactive.png',
+  theme_toolbar: 'toolbar.png',
+  theme_tab_background: 'tab-background.png',
+  theme_tab_background_incognito: 'tab-background-incognito.png',
+};
+
+const renderManifest = colorSet =>
+  Promise.resolve({
+    name: path.join(themeDirectory(colorSet), 'manifest.json'),
+    contents: Buffer.from(
+      JSON.stringify(
+        {
+          version,
+          manifest_version: 2,
+          name: themeName(colorSet),
+          theme: {
+            images,
+            colors: {},
+            tints: {},
+          },
+        },
+        null,
+        2
+      ),
+      'utf8'
+    ),
+  });
+
+const renderImages = colorSet => [
+  colorPixel(colorSet.colors.shade0).then(pixel => ({
+    name: path.join(themeDirectory(colorSet), images.theme_frame),
+    contents: pixel,
+  })),
+];
 
 const render = colors => {
-  const colorSets = [
-    {name: 'Dark', colors: colors.dark && formatColorSet(colors.dark)},
-    {name: 'Light', colors: colors.light && formatColorSet(colors.light)},
-  ].filter(colorSet => !!colorSet.colors);
-  return colorSets.map(colorSet => {
-    const {
-      shade0,
-      shade1,
-      shade2,
-      shade3,
-      shade4,
-      shade5,
-      shade6,
-      shade7,
-      accent0,
-      accent1,
-      accent2,
-      accent3,
-      accent4,
-      accent5,
-      accent6,
-      accent7,
-    } = colorSet.colors;
-    const manifest = {
-      manifest_version: 2,
-      version,
-      name: `Themer ${colorSet.name}`,
-      theme: {
-        colors: {
-          frame: shade0,
-          frame_inactive: shade1,
-          frame_incognito: shade2,
-          frame_incognito_inactive: shade3,
-          toolbar: shade1,
-          tab_text: shade7,
-          tab_background_text: shade5,
-          bookmark_text: shade6,
-        },
-      },
-    };
-    return Promise.resolve({
-      name: path.join(`Themer ${colorSet.name}`, 'manifest.json'),
-      contents: Buffer.from(JSON.stringify(manifest, null, 2), 'utf8'),
-    });
-  });
+  const colorSets = Object.keys(colors).map(key => ({
+    name: capitalize(key),
+    colors: formatColorSet(colors[key]),
+  }));
+  return flattenDeep(
+    colorSets.map(colorSet => [
+      renderManifest(colorSet),
+      renderImages(colorSet),
+    ])
+  );
 };
 
 module.exports = {render};
