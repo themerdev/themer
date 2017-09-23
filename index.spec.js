6ddcc451600bfda7a08dde78ad28db9c75d31053
Matt Swensen <swensen.matthew@gmail.com>
2017-09-22 21:08:08 -0600

    Initial commit

---
+++ index.spec.js
@@ -0,0 +1,7 @@
+const {colors} = require('./index');
+
+describe('themer color set', () => {
+  it('should provide complete dark and light themes', () => {
+    expect(colors).toMatchSnapshot();
+  });
+});
