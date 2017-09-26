2f0006f42c43bfe0ccbfed9e10869c2d027204bc
Matt Swensen <swensen.matthew@gmail.com>
2017-09-26 06:32:32 -0600

    Initial commit.

---
+++ index.spec.js
@@ -0,0 +1,25 @@
+const { render } = require('./index');
+const { colors } = require('themer-colors-default');
+
+describe('themer color set inline preview', () => {
+  it('should use the color set name as the default name', done => {
+    Promise.all(render(colors, { colors: 'themer-colors-default' })).then(files => {
+      expect(files.every(file => /themer-colors-default-(light|dark)-inline\.svg/.test(file.name))).toBe(true);
+      done();
+    });
+  });
+  it('should also accept custom names through options', done => {
+    Promise.all(render(colors, { 'themer-preview-inline-name': 'test', colors: 'themer-colors-default' })).then(files => {
+      expect(files.every(file => /test-(light|dark)-inline\.svg/.test(file.name))).toBe(true);
+      done();
+    });
+  });
+  it('should render proper SVG files', done => {
+    Promise.all(render(colors, { colors: 'themer-colors-default' })).then(files => {
+      files.forEach(file => {
+        expect(file.contents.toString('utf8')).toMatchSnapshot();
+      });
+      done();
+    });
+  });
+});
