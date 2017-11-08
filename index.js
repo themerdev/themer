8d336d41f78b3108fc32f31e2e2cd54c49656d2e
Matt Swensen <swensen.matthew@gmail.com>
2017-11-07 20:04:17 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,20 @@
+exports.colors = {
+  dark: {
+    accent0: '#EF4E7C',
+    accent1: '#F37055',
+    accent2: '#F79532',
+    accent3: '#6EBB82',
+    accent4: '#09B399',
+    accent5: '#1299AD',
+    accent6: '#5073B8',
+    accent7: '#A067AB',
+    shade0: '#23292D',
+    shade1: '#42474B',
+    shade2: '#616668',
+    shade3: '#808486',
+    shade4: '#A0A2A4',
+    shade5: '#BFC0C2',
+    shade6: '#DEDFDF',
+    shade7: '#FDFDFD',
+  },
+};
