a93aa8038d9257afda7bd1d58152e2363bfaec4a
Matt Swensen <swensen.matthew@gmail.com>
2018-09-29 20:10:51 -0600

    DRY up common code

--- packages/themer-wallpaper-octagon/lib/index.js
+++ packages/themer-wallpaper-octagon/lib/index.js
@@ -1,53 +1,19 @@
-const backgroundPatternSize = 36;
-
-const getSizesFromOptOrDefault = opt => {
-  if (opt) {
-    const unparsedSizes = Array.isArray(opt) ? opt : [opt];
-    return unparsedSizes.map(unparsedSize => {
-      const results = /(\d+)x(\d+)/.exec(unparsedSize);
-      if (results) {
-        const w = parseInt(results[1], 10);
-        const h = parseInt(results[2], 10);
-        const s = w / Math.round(w / backgroundPatternSize);
-        return {
-          w: w,
-          h: h,
-          s: s,
-        };
-      }
-      else {
-        throw new Error(`Malformed resolution argument: ${unparsedSize}`);
-      }
-    });
-  }
-  else {
-    return [
-      {
-        w: 2880,
-        h: 1800,
-        s: backgroundPatternSize,
-      },
-      {
-        w: 750,
-        h: 1334,
-        s: backgroundPatternSize,
-      }
-    ];
-  }
-};
+const {
+  getSizesFromOptOrDefault,
+  colorSets: getColorSets,
+  deepFlatten
+} = require('themer-utils');
 
 const render = (colors, options) => {
 
   try {
-    var sizes = getSizesFromOptOrDefault(options['themer-wallpaper-octagon-size']);
+    var sizes = getSizesFromOptOrDefault(options['themer-wallpaper-octagon-size'], 36);
   }
   catch(e) {
     return [Promise.reject(e.message)];
   }
 
-  const colorSets = [{ name: 'dark', colors: colors.dark }, { name: 'light', colors: colors.light }].filter(colorSet => !!colorSet.colors);
-
-  const deepFlatten = arr => arr.reduce((cumulative, inner) => cumulative.concat(Array.isArray(inner) ? deepFlatten(inner) : inner), []);
+  const colorSets = getColorSets(colors);
 
   const getOctagonPathData = size => `
   M ${size/6} ${0}
