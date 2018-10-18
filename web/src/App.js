4c21fb61833923a5b30d5d35e258b2c5c7df5702
Matt Swensen <swensen.matthew@gmail.com>
2018-10-17 20:49:44 -0600

    Better color state API.

--- web/src/App.js
+++ web/src/App.js
@@ -2,26 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import { UrlStateProvider } from './UrlState';
 
-import BackgroundColor from './BackgroundColor';
-import ForegroundColor from './ForegroundColor';
+import Color from './Color';
 
 export default class App extends Component {
   render() {
     return (
       <UrlStateProvider history={ this.props.history }>
-        <BackgroundColor>
-          { backgroundColor => (
-            <div className="app" style={{ backgroundColor }}>
+        <Color>
+          { getColor => (
+            <div className="app" style={{ backgroundColor: getColor('shade0') }}>
               <div className="container">
-                <ForegroundColor>
-                  { color => (
-                    <h1 style={{ color }}>[logo] themer</h1>
-                  ) }
-                </ForegroundColor>
+                <h1 style={{ color: getColor('shade7'), borderBottomColor: getColor('shade4', 'shade7') }}>themer</h1>
+                <p style={{ color: getColor('shade6', 'shade7')}}>themer takes a color set and generates...</p>
               </div>
             </div>
           ) }
-        </BackgroundColor>
+        </Color>
       </UrlStateProvider>
     );
   }
--- web/src/Color.js
+++ web/src/Color.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { UrlStateConsumer } from './UrlState';
 
-export default function Color ({ children, colorKeys }) {
-  return (
-    <UrlStateConsumer>
-      { ({ getValueOrFallback }) => children(
-        getValueOrFallback(
-          colorKeys.map(colorKey => [
-            'colors',
-            getValueOrFallback([['activeColorSet']]),
-            colorKey
-          ]),
-          true,
-        )
-      ) }
-    </UrlStateConsumer>
-  );
+export default class Color extends PureComponent {
+  render() {
+    return (
+      <UrlStateConsumer>
+        { ({ getValueOrFallback }) => this.props.children(
+          (...args) => getValueOrFallback(
+            [...args].map(colorKey => [
+              'colors',
+              getValueOrFallback([['activeColorSet']]),
+              colorKey
+            ]),
+            true,
+          )
+        ) }
+      </UrlStateConsumer>
+    );
+  }
 }
