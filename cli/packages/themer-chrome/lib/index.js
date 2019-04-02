0ec2513d142a239ec592b341e71d0a5a9f854277
Matt Swensen <swensen.matthew@gmail.com>
2019-04-02 06:36:53 -0600

    Update Chrome theme for newer versions of Chrome.

--- cli/packages/themer-chrome/lib/index.js
+++ cli/packages/themer-chrome/lib/index.js
@@ -3,23 +3,16 @@ const {capitalize, mapValues} = require('lodash');
 const Color = require('color');
 const {version} = require('../package.json');
 
-const formatColorSet = colors =>
+const rgbColorSet = colors =>
   mapValues(colors, hex => Color(hex).rgb().array());
+
+const hslColorSet = colors =>
+  mapValues(colors, hex => Color(hex).hsl().array());
 
 const themeName = colorSet => `Themer ${colorSet.name}`;
 const themeDirectory = themeName;
 
-const renderManifest = colorSet => {
-  const {
-    shade0,
-    shade1,
-    shade2,
-    shade3,
-    shade4,
-    shade5,
-    shade6,
-    shade7,
-  } = colorSet.colors;
+const renderManifest = (colorSet) => {
   return Promise.resolve({
     name: path.join(themeDirectory(colorSet), 'manifest.json'),
     contents: Buffer.from(
@@ -30,18 +23,22 @@ const renderManifest = colorSet => {
           name: themeName(colorSet),
           theme: {
             colors: {
-              frame: colorSet.isDark ? shade0 : shade1,
-              frame_inactive: colorSet.isDark ? shade1 : shade0,
-              frame_incognito: colorSet.isDark ? shade3 : shade4,
-              frame_incognito_inactive: colorSet.isDark ? shade4 : shade3,
-              toolbar: colorSet.isDark ? shade1 : shade0,
-              tab_text: shade7,
-              tab_background_text: shade5,
-              bookmark_text: shade6,
-              ntp_background: shade0,
-              ntp_text: shade6,
-              ntp_section: shade2,
-              ntp_section_text: shade7,
+              bookmark_text: colorSet.rgbColors.shade7,
+              frame: colorSet.isDark ? colorSet.rgbColors.shade0 : colorSet.rgbColors.shade1,
+              ntp_background: colorSet.isDark ? colorSet.rgbColors.shade1 : colorSet.rgbColors.shade0,
+              ntp_text: colorSet.rgbColors.shade7,
+              tab_background_text: colorSet.rgbColors.shade7,
+              tab_background_text_inactive: colorSet.rgbColors.shade7,
+              tab_background_text_incognito: colorSet.rgbColors.shade7,
+              tab_background_text_incognito_inactive: colorSet.rgbColors.shade7,
+              tab_text: colorSet.rgbColors.shade7,
+              toolbar: colorSet.isDark ? colorSet.rgbColors.shade1 : colorSet.rgbColors.shade0,
+            },
+            tints: {
+              buttons: [colorSet.hslColors.accent5[0] / 360, 0.5, 0.5],
+              frame_inactive: [-1, -1, -1],
+              frame_incognito: [colorSet.hslColors.accent6[0] / 360, 1.0, colorSet.isDark ? 0.55 : 0.45],
+              frame_incognito_inactive: [colorSet.hslColors.accent6[0] / 360, 1.0, colorSet.isDark ? 0.55 : 0.45],
             },
           },
         },
@@ -56,7 +53,8 @@ const renderManifest = colorSet => {
 const render = colors => {
   const colorSets = Object.keys(colors).map(key => ({
     name: capitalize(key),
-    colors: formatColorSet(colors[key]),
+    rgbColors: rgbColorSet(colors[key]),
+    hslColors: hslColorSet(colors[key]),
     isDark: key === 'dark',
   }));
   return colorSets.map(colorSet => renderManifest(colorSet));
