7ed10c161f1c286cb850657090253f2bae81ada4
Matt Swensen <swensen.matthew@gmail.com>
2020-04-18 07:35:55 -0600

    Add template for Alacritty terminal emulator
    Resolves #40

---
+++ cli/packages/themer-alacritty/README.md
@@ -0,0 +1,13 @@
+# themer-alacritty
+
+An [alacritty](https://github.com/alacritty/alacritty) theme template for [themer](https://github.com/mjswensen/themer).
+
+## Installation & usage
+
+Install this module wherever you have `themer` installed:
+
+    npm install themer-alacritty
+
+Then pass `themer-alacritty` as a `-t` (`--template`) arg to `themer`:
+
+    themer -c my-colors.js -t themer-alacritty -o gen
---
+++ cli/packages/themer-alacritty/lib/index.spec.js
@@ -0,0 +1,29 @@
+const { render, renderInstructions } = require('./index');
+const { colors } = require('../../themer-colors-default');
+const singleThemeColors = { light: colors.light };
+
+describe('themer themer-alacritty theme generator', () => {
+
+  const promisedFilesDefault = Promise.all(render(colors));
+  const promisedFilesSingleTheme = Promise.all(render(singleThemeColors));
+  
+  it('should generate a single file regardless of dark, light, or both', async () => {
+    const filesDefault = await promisedFilesDefault;
+    expect(filesDefault.length).toBe(1);
+    const filesSingleTheme = await promisedFilesSingleTheme;
+    expect(filesSingleTheme.length).toBe(1);
+  });
+  
+  it('should generate well-formatted themes', async () => {
+    const filesDefault = await promisedFilesDefault;
+    expect(filesDefault[0].contents.toString('utf8')).toMatchSnapshot();
+    const filesSingleTheme = await promisedFilesSingleTheme;
+    expect(filesSingleTheme[0].contents.toString('utf8')).toMatchSnapshot();
+  });
+
+  it('should provide installation instructions', async () => {
+    const files = await Promise.all(render(colors));
+    const instructions = renderInstructions(files.map(({ name }) => name));
+    expect(instructions).toMatchSnapshot();
+  });
+});
