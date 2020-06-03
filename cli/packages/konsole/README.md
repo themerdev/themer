bbc3103d022eb71ffd972dcbc1277ca032b35e05
Matt Swensen <swensen.matthew@gmail.com>
2020-06-03 05:57:04 -0600

    Implement Konsole theme generator

---
+++ cli/packages/konsole/README.md
@@ -0,0 +1,15 @@
+# @themer/konsole
+
+A Konsole template for [themer](https://github.com/mjswensen/themer).
+
+## Installation & usage
+
+Install this module wherever you have `themer` installed:
+
+    npm install @themer/konsole
+
+Then pass `@themer/konsole` as a `-t` (`--template`) arg to `themer`:
+
+    themer -c my-colors.js -t @themer/konsole -o gen
+
+Installation instructions for the generated theme(s) will be included in `<output dir>/README.md`.
---
+++ cli/packages/konsole/lib/index.spec.js
@@ -0,0 +1,19 @@
+const { render, renderInstructions } = require('./index');
+const { colors } = require('../../colors-default');
+
+describe('themer konsole theme generator', () => {
+  const promisedFiles = Promise.all(render(colors));
+
+  it('should generate well-formatted themes', async () => {
+    const files = await promisedFiles;
+    for (const file of files) {
+      expect(file.contents.toString('utf8')).toMatchSnapshot();
+    }
+  });
+
+  it('should provide installation instructions', async () => {
+    const files = await promisedFiles;
+    const instructions = renderInstructions(files.map(({ name }) => name));
+    expect(instructions).toMatchSnapshot();
+  });
+});
