8828fa35e3b4eab98eee622e38a87bf0a170e8a9
Matt Swensen <swensen.matthew@gmail.com>
2020-06-04 06:15:19 -0600

    Add new color set

---
+++ cli/packages/colors-green-as-a-whistle/README.md
@@ -0,0 +1,13 @@
+# @themer/colors-green-as-a-whistle
+
+A warm [themer](https://github.com/mjswensen/themer) color set featuring desaturated shades of green.
+
+## Installation & usage
+
+Install this module wherever you have `themer` installed:
+
+    npm install @themer/colors-green-as-a-whistle
+
+Then pass `@themer/colors-green-as-a-whistle` as a `-c` (`--colors`) arg to `themer`:
+
+    themer -c @themer/colors-green-as-a-whistle ...
---
+++ cli/packages/colors-green-as-a-whistle/lib/index.spec.js
@@ -0,0 +1,22 @@
+const { colors } = require('./index');
+
+describe('@themer/colors-green-as-a-whistle', () => {
+  it('should define all required colors', () => {
+    const prefixes = [
+      'accent0',
+      'accent1',
+      'accent2',
+      'accent3',
+      'accent4',
+      'accent5',
+      'accent6',
+      'accent7',
+      'shade0',
+      'shade7',
+    ]
+    prefixes.forEach(prefix => {
+      expect(colors.dark[prefix]).toBeDefined();
+      expect(colors.light[prefix]).toBeDefined();
+    });
+  });
+});
