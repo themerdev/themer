736cf3236a3ff1e911eadbd871939f1d69f126cb
Matt Swensen <swensen.matthew@gmail.com>
2020-06-23 17:13:27 -0600

    Implement Visual Studio theme template

---
+++ cli/packages/visual-studio/README.md
@@ -0,0 +1,15 @@
+# @themer/visual-studio
+
+A Visual Studio template for [themer](https://github.com/mjswensen/themer).
+
+## Installation & usage
+
+Install this module wherever you have `themer` installed:
+
+    npm install @themer/visual-studio
+
+Then pass `@themer/visual-studio` as a `-t` (`--template`) arg to `themer`:
+
+    themer -c my-colors.js -t @themer/visual-studio -o gen
+
+Installation instructions for the generated theme(s) will be included in `<output dir>/README.md`.
---
+++ cli/packages/visual-studio/lib/index.spec.js
@@ -0,0 +1,20 @@
+const { render, renderInstructions } = require('./index');
+const { colors } = require('../../colors-default');
+
+describe('themer visual-studio theme generator', () => {
+  it('should render valid theme files', async () => {
+    const files = await Promise.all(render(colors));
+    expect(files.length).toBe(2);
+    files.forEach(file => {
+      expect(file.contents.toString('utf8')).toMatchSnapshot();
+    });
+  });
+});
+
+describe('renderInstructions', () => {
+  it('should provide installation instructions', async () => {
+    const files = await Promise.all(render(colors));
+    const instructions = renderInstructions(files.map(({ name }) => name));
+    expect(instructions).toMatchSnapshot();
+  });
+});
