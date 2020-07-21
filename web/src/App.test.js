657b4f671768a64af04d31ce595976bbfb19c1f1
Matt Swensen <swensen.matthew@gmail.com>
2020-07-21 09:16:19 -0600

    Fix test

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -5,6 +5,7 @@ import App from './App';
 import { createMemoryHistory } from 'history';
 
 it('renders without crashing', () => {
+  window.__ssa__log = () => {};
   const div = document.createElement('div');
   ReactDOM.render(<App history={ createMemoryHistory() } />, div);
   ReactDOM.unmountComponentAtNode(div);
