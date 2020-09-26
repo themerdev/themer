5bdc77e50ee5730154ef30be97de9127dbaad1fb
Matt Swensen <swensen.matthew@gmail.com>
2020-09-26 06:39:24 -0600

    Upgrade CLI dependencies

--- cli/packages/wallpaper-trianglify/lib/index.js
+++ cli/packages/wallpaper-trianglify/lib/index.js
@@ -1,4 +1,4 @@
-const Trianglify = require('trianglify'),
+const trianglify = require('trianglify'),
   { deepFlatten, listOutputFiles } = require('@themer/utils'),
   { getSizes, getVariances } = require('./opts');
 
@@ -16,25 +16,25 @@ const render = (colors, options) => {
       colorSets.map(([colorSetName, colors]) =>
         [
           {
-            x_colors: [ colors.accent4, colors.accent6 ],
-            y_colors: [ colors.shade7, colors.shade0 ],
+            xColors: [ colors.accent4, colors.accent6 ],
+            yColors: [ colors.shade7, colors.shade0 ],
           },
           {
-            x_colors: [ colors.accent2, colors.accent7 ],
-            y_colors: [ colors.shade7, colors.shade0 ],
+            xColors: [ colors.accent2, colors.accent7 ],
+            yColors: [ colors.shade7, colors.shade0 ],
           },
         ].map((configuration, configurationIdx) =>
           variances.map(variance =>
             sizes.map(size =>
               new Promise((resolve, reject) => {
                 try {
-                  const pattern = new Trianglify({
+                  const pattern = trianglify({
                     width: size.w,
                     height: size.h,
                     variance,
                     ...configuration,
                   });
-                  const svgString = pattern.svg({includeNamespace: true}).outerHTML;
+                  const svgString = pattern.toSVGTree({includeNamespace: true}).toString();
                   resolve({
                     name: `themer-wallpaper-trianglify-${colorSetName}-${size.w}x${size.h}-${variance}-${configurationIdx+1}.svg`,
                     contents: Buffer.from(svgString, 'utf8'),
--- cli/packages/wallpaper-trianglify/lib/index.spec.js
+++ cli/packages/wallpaper-trianglify/lib/index.spec.js
@@ -7,8 +7,8 @@ describe('themer trianglify wallpaper', () => {
       'themer-wallpaper-trianglify-size': '200x200',
     }));
     files.forEach(file => {
-      expect(/width="200"/.test(file.contents.toString('utf8'))).toBe(true);
-      expect(/height="200"/.test(file.contents.toString('utf8'))).toBe(true);
+      expect(/width='200'/.test(file.contents.toString('utf8'))).toBe(true);
+      expect(/height='200'/.test(file.contents.toString('utf8'))).toBe(true);
     });
   });
   it('should allow for specifying variances', async () => {
