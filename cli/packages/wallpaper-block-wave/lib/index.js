31a17e7c940ddda6d55967a7a03d3e34efec32a6
Matt Swensen <swensen.matthew@gmail.com>
2020-10-10 18:40:35 -0600

    Output png instead of svg for block wave wallpaper

--- cli/packages/wallpaper-block-wave/lib/index.js
+++ cli/packages/wallpaper-block-wave/lib/index.js
@@ -5,6 +5,8 @@ const {
   listOutputFiles,
 } = require('@themer/utils');
 const weightedRandom = require('./weighted-random');
+const { createCanvas } = require('canvas');
+const Color = require('color');
 
 const render = (colors, options) => {
 
@@ -37,55 +39,79 @@ const render = (colors, options) => {
   const colorSets = getColorSets(colors);
 
   return deepFlatten(
-    colorSets.map(colorSet => sizes.map(size => {
-      let blockMaxSize = size.s / 3;
-      let blockMinSize = blockMaxSize * 2/3;
-      let blocks = [];
-      for (let i = 0; i < size.w; i += size.s) {
-        for (let j = 0; j < size.h; j += size.s) {
-          let color = colorSet.colors[weightedRandom(colorWeights)];
-          let xPosition = (i + size.s / 2) / size.w;
-          let yPosition = (j + size.s / 2) / size.h;
-          let positionScaleFactor = Math.abs(xPosition - yPosition);
-          let blockSize = blockMaxSize - (blockMaxSize - blockMinSize) * positionScaleFactor;
-          let padding = (size.s - blockSize) / 2;
-          blocks.push({
-            x: i + padding,
-            y: j + padding,
-            w: blockSize,
-            h: blockSize,
-            c: color,
-            g: Math.random() < 0.01,
-          });
-        }
-      }
-      return { size: size, blocks: blocks };
-    }).map(svgData => Promise.resolve({
-      name: `themer-wallpaper-block-wave-${colorSet.name}-${svgData.size.w}x${svgData.size.h}.svg`,
-      contents: Buffer.from(`
-        <svg xmlns="http://www.w3.org/2000/svg" width="${svgData.size.w}" height="${svgData.size.h}" viewBox="0 0 ${svgData.size.w} ${svgData.size.h}">
-          <defs>
-            <linearGradient id="overlay" x1="1" y1="0" x2="0" y2="1">
-              <stop offset="0%" stop-color="${colorSet.colors.shade0}"/>
-              <stop offset="50%" stop-color="${colorSet.colors.shade0}" stop-opacity="0"/>
-              <stop offset="100%" stop-color="${colorSet.colors.shade0}"/>
-            </linearGradient>
-            <filter id="glow">
-              <feGaussianBlur stdDeviation="5" result="coloredBlur"/>
-              <feMerge>
-                <feMergeNode in="coloredBlur"/>
-                <feMergeNode in="SourceGraphic"/>
-              </feMerge>
-            </filter>
-          </defs>
-          <rect x="0" y="0" width="${svgData.size.w}" height="${svgData.size.h}" fill="${colorSet.colors.shade0}" />
-          ${svgData.blocks.map(block => `<rect x="${block.x}" y="${block.y}" width="${block.w}" height="${block.h}" fill="${block.c}" rx="2" ry="2" ${block.g ? 'filter="url(#glow)"' : ''} />`).join('\n')}
-          <rect x="0" y="0" width="${svgData.size.w}" height="${svgData.size.h}" fill="url(#overlay)"/>
-        </svg>
-      `, 'utf8'),
-    })))
-  );
+    colorSets.map((colorSet) =>
+      sizes
+        .map((size) => {
+          let blockMaxSize = size.s / 3;
+          let blockMinSize = blockMaxSize * 2/3;
+          let blocks = [];
+          for (let i = 0; i < size.w; i += size.s) {
+            for (let j = 0; j < size.h; j += size.s) {
+              let color = colorSet.colors[weightedRandom(colorWeights)];
+              let xPosition = (i + size.s / 2) / size.w;
+              let yPosition = (j + size.s / 2) / size.h;
+              let positionScaleFactor = Math.abs(xPosition - yPosition);
+              let blockSize = blockMaxSize - (blockMaxSize - blockMinSize) * positionScaleFactor;
+              let padding = (size.s - blockSize) / 2;
+              blocks.push({
+                x: i + padding,
+                y: j + padding,
+                w: blockSize,
+                h: blockSize,
+                c: color,
+                g: Math.random() < 0.01,
+              });
+            }
+          }
+          return { size: size, blocks: blocks };
+        })
+        .map(async ({ size, blocks }) => {
+          const canvas = createCanvas(size.w, size.h);
+          const ctx = canvas.getContext('2d');
 
+          ctx.fillStyle = colorSet.colors.shade0;
+          ctx.fillRect(0, 0, size.w, size.h);
+
+          blocks.forEach(({ x, y, w, h, c, g }) => {
+            const radius = 2;
+            const right = x + w;
+            const bottom = y + h;
+            ctx.fillStyle = c;
+            ctx.shadowColor = c;
+            ctx.shadowBlur = g ? 5 : 0;
+            ctx.beginPath();
+            ctx.moveTo(x + radius, y);
+            ctx.lineTo(right - radius, y);
+            ctx.quadraticCurveTo(right, y, right, y + radius);
+            ctx.lineTo(right, y + h - radius);
+            ctx.quadraticCurveTo(right, bottom, right - radius, bottom);
+            ctx.lineTo(x + radius, bottom);
+            ctx.quadraticCurveTo(x, bottom, x, bottom - radius);
+            ctx.lineTo(x, y + radius);
+            ctx.quadraticCurveTo(x, y, x + radius, y);
+            ctx.fill();
+          });
+
+          const gradient = ctx.createLinearGradient(size.w, 0, 0, size.h);
+          gradient.addColorStop(0, colorSet.colors.shade0);
+          gradient.addColorStop(
+            0.5,
+            Color(colorSet.colors.shade0).alpha(0).rgb().string()
+          );
+          gradient.addColorStop(1, colorSet.colors.shade0);
+          ctx.fillStyle = gradient;
+          ctx.fillRect(0, 0, size.w, size.h);
+
+          return {
+            name: `themer-wallpaper-block-wave-${colorSet.name}-${size.w}x${size.h}.png`,
+            contents: Buffer.from(
+              canvas.toDataURL().replace('data:image/png;base64,', ''),
+              'base64'
+            ),
+          };
+        })
+    )
+  );
 };
 
 module.exports = {
