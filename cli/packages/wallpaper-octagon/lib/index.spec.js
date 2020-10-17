dfca4c00ed06c520b9d03a6450198b60688c35fd
Matt Swensen <swensen.matthew@gmail.com>
2020-10-17 13:30:35 -0600

    Output png instead of svg for octagon wallpaper

--- cli/packages/wallpaper-octagon/lib/index.spec.js
+++ cli/packages/wallpaper-octagon/lib/index.spec.js
@@ -13,14 +13,7 @@ describe('themer "octagon" wallpaper', () => {
         it(`should return ${totalDefaultFiles} files to write`, async () => {
           const files = await Promise.all(promises);
           expect(files.length).toBe(totalDefaultFiles);
-          expect(files.filter(file => /\.svg/.test(file.name)).length).toBe(totalDefaultFiles);
-        });
-
-        it('should not contain "undefined" in the SVG output', async () => {
-          const files = await Promise.all(promises);
-          files.filter(file => /\.svg/.test(file.name)).forEach(file => {
-            expect(file.contents.toString('utf8')).not.toContain('undefined');
-          });
+          expect(files.filter(file => /\.png/.test(file.name)).length).toBe(totalDefaultFiles);
         });
 
       });
@@ -31,14 +24,7 @@ describe('themer "octagon" wallpaper', () => {
         it(`should return ${totalDefaultFiles / 2} files to write`, async () => {
           const files = await Promise.all(promises);
           expect(files.length).toBe(totalDefaultFiles / 2);
-          expect(files.filter(file => /\.svg/.test(file.name)).length).toBe(totalDefaultFiles / 2);
-        });
-
-        it('should not contain "undefined" in the SVG output', async () => {
-          const files = await Promise.all(promises);
-          files.filter(file => /\.svg/.test(file.name)).forEach(file => {
-            expect(file.contents.toString('utf8')).not.toContain('undefined');
-          });
+          expect(files.filter(file => /\.png/.test(file.name)).length).toBe(totalDefaultFiles / 2);
         });
 
       });
