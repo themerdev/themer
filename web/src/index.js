11887e3cca114cc5d3b327b161a05c2d9ba6efff
Matt Swensen <swensen.matthew@gmail.com>
2021-06-19 16:50:54 +0000

    Remove Sentry

--- web/src/index.js
+++ web/src/index.js
@@ -5,13 +5,6 @@ import 'reset.css/reset.css';
 import './index.css';
 import App from './App';
 import { register } from './serviceWorkerRegistration';
-import * as Sentry from '@sentry/browser';
-
-Sentry.init({
-  dsn: 'https://9f43906c1ad243f4a3ee46f26e1ad0b2@o390880.ingest.sentry.io/5236092',
-  environment: process.env.NODE_ENV,
-  release: process.env.REACT_APP_VERSION,
-});
 
 const history = createBrowserHistory();
 
